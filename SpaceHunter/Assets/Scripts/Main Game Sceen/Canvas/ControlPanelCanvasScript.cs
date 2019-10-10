db96648765b19e552d8bc53e2fed58c3489ba147
RadFam <artkorkh@gmail.com>
2019-10-11 01:48:04 +0300

    game UI added

---
+++ SpaceHunter/Assets/Scripts/Main Game Sceen/Canvas/ControlPanelCanvasScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlPanelCanvasScript : MonoBehaviour {
+
+    public Slider fuelLevel;
+    public Slider healthLevel;
+    public Slider plasmaLevel;
+
+    // Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
