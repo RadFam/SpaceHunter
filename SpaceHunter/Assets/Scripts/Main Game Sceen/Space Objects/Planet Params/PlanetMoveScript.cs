a0f1bc0ecb68763bbd43d01849659acdd5a5a6ae
RadFam <artkorkh@gmail.com>
2019-10-13 23:07:58 +0300

    .... Not working

--- SpaceHunter/Assets/Scripts/Main Game Sceen/Space Objects/Planet Params/PlanetMoveScript.cs
+++ SpaceHunter/Assets/Scripts/Main Game Sceen/Space Objects/Planet Params/PlanetMoveScript.cs
@@ -15,12 +15,34 @@ public class PlanetMoveScript : MonoBehaviour {
     public float bigSemiAxis;
     public float smallSemiAxis;
     public float orbitalRevolutionSpeed;
+
+    public List<Vector3> posToWalk;
+    private float cParam;
+    private int pointCount;
+    private Vector3 pointCoord;
+
+    void Awake()
+    {
+        cParam = Mathf.Sqrt(bigSemiAxis * bigSemiAxis - smallSemiAxis * smallSemiAxis);
+        //Vector3 ellipseCenterVect = Vector3.Normalize(systemCenter - transform.position)*cParam;
+        initPosition = transform.position;
+        Vector3 ellipseCenter = Vector3.Normalize(systemCenter - transform.position) * bigSemiAxis;
+        posToWalk = new List<Vector3>();
+        for (int i = 0; i < 100; ++i)
+        {
+            Vector3 tmp = new Vector3(ellipseCenter.x + smallSemiAxis * Mathf.Sin(i * 2 * 3.14f / 100), ellipseCenter.y, ellipseCenter.z + bigSemiAxis * Mathf.Cos(i * 2 * 3.14f / 100));
+            posToWalk.Add(tmp);
+        }
+
+        pointCount = 1;
+        pointCoord = posToWalk[pointCount];
+    }
     
     // Use this for initialization
 	void Start () 
     {
-        transform.position = initPosition;
-        transform.eulerAngles = new Vector3(polarAxisInclinationX, 0.0f, polarAxisInclinationZ);
+        //transform.position = initPosition;
+        //transform.eulerAngles = new Vector3(polarAxisInclinationX, 0.0f, polarAxisInclinationZ);
 
         // By default, Sun is in point (0, 0, 0)
 	}
@@ -28,7 +50,19 @@ public class PlanetMoveScript : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
     {
-        transform.RotateAround(transform.position, transform.up, selfRotationSpeed * Time.deltaTime);
-        
+        //transform.RotateAround(transform.position, transform.up, selfRotationSpeed * Time.deltaTime);
+
+        float step = orbitalRevolutionSpeed * Time.deltaTime; // calculate distance to move
+        transform.position = Vector3.MoveTowards(transform.position, pointCoord, step);
+
+        if (Vector3.Distance(transform.position, pointCoord) < 1.0f)
+        {
+            pointCount++;
+            if (pointCount >= 100)
+            {
+                pointCount = 0;
+            }
+            pointCoord = posToWalk[pointCount];
+        }
 	}
 }
---
+++ SpaceHunter/Assets/Scripts/Main Game Sceen/Space Objects/Planet Params/PlanetSelfSpin.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetSelfSpin : MonoBehaviour {
+
+    private PlanetMoveScript PMS;
+
+    private float pAIX;
+    private float pAIZ;
+    private float sRS;
+    private Vector3 initPos;
+
+	// Use this for initialization
+	void Start () 
+    {
+	    PMS = GetComponentInParent<PlanetMoveScript>();
+
+        pAIX = PMS.polarAxisInclinationX;
+        pAIZ = PMS.polarAxisInclinationZ;
+        sRS = PMS.selfRotationSpeed;
+
+        initPos = new Vector3(0.0f, 0.0f, 0.0f);
+        transform.localPosition = initPos;
+        transform.localEulerAngles = new Vector3(-90+pAIX, 0.0f, pAIZ);
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () 
+    {
+        transform.RotateAround(gameObject.transform.localPosition, gameObject.transform.up, sRS * Time.deltaTime);
+	}
+}
