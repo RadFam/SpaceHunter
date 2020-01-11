fd37c6e9fbaa7ed8fa122f04f32a1c24b27793c6
RadFam <artkorkh@gmail.com>
2020-01-11 23:10:03 +0300

    some small mods...

---
+++ SpaceHunter/Assets/Scripts/Main Game Sceen/Space Objects/ListPathPoints.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ListPathPoints : MonoBehaviour
+{
+    [Serializable]
+    public struct AllWayPaths
+    {
+        public int pathNum;
+        public List<Transform> pathPoints;
+
+        public AllWayPaths(int num, List<Transform> trnsfrm)
+        {
+            pathNum = num;
+            pathPoints = new List<Transform>();
+            foreach (Transform trn in trnsfrm)
+            {
+                pathPoints.Add(trn);
+            }
+        }
+    }
+
+    [Header("Number of path / List of points")]
+    public List<AllWayPaths> allPaths = new List<AllWayPaths>();
+    private int keyNum = 0;
+
+    public void InputPath(List<Transform> points)
+    {
+        allPaths.Add(new AllWayPaths(keyNum, points));
+        keyNum++;
+    }
+
+    public List<Transform> GetPath(int num) => allPaths[num].pathPoints;
+}
