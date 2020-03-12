9213a702a31f32571d7b59f5b65eb8293dcc980a
AleksandraYakupova <maxlex047@gmail.com>
2020-03-13 01:08:18 +0300

    Some scripts for menu panels

---
+++ SpaceHunter/Assets/Scripts/Menu Game Screen/Canvas/SubPanels/LoadSubPanelScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoadSubPanelScript : MonoBehaviour
+{
+    public 
+
+    public void OnEnable()
+    {
+        
+    }
+
+    public void OnOkButtonPressed()
+    {
+        
+    }
+
+    public void OnExitButtonPressed()
+    {
+        gameObject.SetActive(false);
+    }
+}
---
+++ SpaceHunter/Assets/Scripts/Menu Game Screen/Canvas/SubPanels/SaveSubPanelScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SaveSubPanelScript : MonoBehaviour
+{
+    public Text editText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void OnOkButtonPressed()
+    {
+        if (editText.text != "")
+        {
+            string saveFilename = editText.text;
+        }
+    }
+
+    public void OnExitButtonPressed()
+    {
+        gameObject.SetActive(false);
+    }
+}
