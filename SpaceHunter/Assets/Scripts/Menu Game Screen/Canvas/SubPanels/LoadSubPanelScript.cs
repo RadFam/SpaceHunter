6410dbe5a25c6a61f181cbc18275e65cb7fc9d17
RadFam <artkorkh@gmail.com>
2020-03-13 23:58:39 +0300

    Load saved files

--- SpaceHunter/Assets/Scripts/Menu Game Screen/Canvas/SubPanels/LoadSubPanelScript.cs
+++ SpaceHunter/Assets/Scripts/Menu Game Screen/Canvas/SubPanels/LoadSubPanelScript.cs
@@ -1,15 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class LoadSubPanelScript : MonoBehaviour
 {
-    public 
+    public LoadTextElement loadElementPrefab;
+
+    private string fileExt = "*.shs";
+    private int fileCounts = 0;
+    private int fileChosen = 0;
+    private List<LoadTextElement> loads = new List<LoadTextElement>();
 
     public void OnEnable()
     {
-        
+        string[] fileEntries = Directory.GetFiles("/Saves", fileExt);
+        fileCounts = fileEntries.Length;
+
+        if (fileCounts > 0)
+        {
+            for (int i = 0; i < fileCounts; ++i)
+            {
+                LoadTextElement obj = Instantiate(loadElementPrefab);
+                obj.gameObject.SetActive(true);
+                obj.transform.parent = loadElementPrefab.transform.parent;
+                string tmp = fileEntries[i];
+                tmp = tmp.Replace(".shs", string.Empty);
+                obj.SetParams(tmp, i);
+                loads.Add(obj);
+            }
+        }
+    }
+
+    public void OnLoadFilePressed(int num)
+    {
+        fileChosen = num;
+        for (int i = 0; i < fileCounts; ++i)
+        {
+            if (i != fileChosen)
+            {
+                loads[i].SetColor(1);
+            }
+            else
+            {
+                loads[i].SetColor(2);
+            }
+        }
     }
 
     public void OnOkButtonPressed()
@@ -19,6 +56,7 @@ public class LoadSubPanelScript : MonoBehaviour
 
     public void OnExitButtonPressed()
     {
+        loads.Clear();
         gameObject.SetActive(false);
     }
 }
---
+++ SpaceHunter/Assets/Scripts/Menu Game Screen/Canvas/SubPanels/LoadTextElement.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.EventSystems;
+
+public class LoadTextElement : MonoBehaviour, IPointerClickHandler
+{
+    // Start is called before the first frame update
+    public TextMeshProUGUI loadText;
+    private int myNum;
+
+    public int MyNum
+    {
+        get { return myNum; }
+        set { myNum = value; }
+    }
+
+   public void SetParams(string txt, int num)
+   {
+        loadText.text = txt;
+        myNum = num;
+   }
+
+    public void SetColor(int colorType)
+    {
+        Color color;
+        if (colorType == 1)
+        {
+            color = new Color(255, 246, 0);
+        }
+        if (colorType == 2)
+        {
+            color = new Color(255, 255, 255);
+        }
+    }
+
+    public void OnPointerClick(PointerEventData pointerEventData)
+    {
+        LoadSubPanelScript lsps = GameObject.Find("LoadSubPanelScript").GetComponent<LoadSubPanelScript>();
+        lsps.OnLoadFilePressed(myNum);
+    }
+}
